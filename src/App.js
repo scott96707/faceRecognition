b7db24f136bf76afca191033deb32829cb112d88
scott96707 <scott96707@gmail.com>
2019-01-19 15:08:43 -0700

    Fixing fetch links

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('https://nameless-bayou-51714.herokuapp.com:3000/imageurl', {
+      fetch('https://nameless-bayou-51714.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://nameless-bayou-51714.herokuapp.com:3000/image', {
+          fetch('https://nameless-bayou-51714.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
