c660c8d2dc3d8bd8326d6854467424ad6f146bfb
scott96707 <scott96707@gmail.com>
2019-05-08 17:08:20 -0600

    Again, fixing bug for multiple face detection

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const particlesOptions ={
               "duration": 4
           }
       }
-	    }
+    }
 }
 const initialState = {
   input: '',
@@ -91,20 +91,20 @@ class App extends Component {
     }})
   }
 
-  calculateFaceLocations = (data) => {
-      return data.outputs[0].data.regions.map(face =>{
-        const clarifaiFace = face.region_info.bounding_box;
-        const image = document.getElementById('inputimage');
-        const width = Number(image.width);
-        const height = Number(image.width);
-        return {
-          leftCol: clarifaiFace.left_col * width,
-          topRow: clarifaiFace.top_row * height,
-          rightCol: width - (clarifaiFace.right_col * width),
-          bottomRow: height - (clarifaiFace.bottom_row * height),
-        }
-      });
-  }
+  calculateFaceLocation = (data) => {
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return data.outputs[0].data.regions.map(face => {
+      const clarifaiFace = face.region_info.bounding_box;
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    });
+}
 
   displayFaceBox = (boxes) => {
     this.setState({boxes: boxes});
